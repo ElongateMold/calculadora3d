be0a9244048d1de8e913fe32e9a38e82fd095504
ElongateMold <stefan.lazo.rilling@gmail.com>
2026-05-25 20:27:40 -0400

    Reparado el sistema de almacenamiento de datos en Firebase

--- lib/repositories/history_repository.dart
+++ lib/repositories/history_repository.dart
@@ -1,5 +1,6 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 // Separación de clases 
 // Solo define qué hace
@@ -13,29 +14,54 @@ abstract class HistoryRepository {
 class FirebaseHistory implements HistoryRepository {
   @override
   Future<void> savePrint(Map<String, dynamic> data) async {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid == null) return;
+    
     final db = FirebaseFirestore.instance;
-    await db.collection('history').add(data);
+    await db
+        .collection('users')
+        .doc(uid)
+        .collection('history')
+        .add(data);
   }
 
   @override
-  Future<void> deletePrint(String id) async {
-    final db = FirebaseFirestore.instance;
-    await db.collection('history').doc(id).delete();
-  }
+Future<void> deletePrint(String id) async {
+  final uid = FirebaseAuth.instance.currentUser?.uid;
+  if (uid == null) return;
+
+  final db = FirebaseFirestore.instance;
+  await db
+      .collection('users')
+      .doc(uid)
+      .collection('history')
+      .doc(id)
+      .delete();
+}
   
   @override
-  Future<List<Map<String, dynamic>>> readPrint() async {
-    final db = FirebaseFirestore.instance;
-    final snapshot = await db.collection('history').orderBy('date', descending: true).get();
-    List<Map<String, dynamic>> listHistory = [];
+Future<List<Map<String, dynamic>>> readPrint() async {
+  final uid = FirebaseAuth.instance.currentUser?.uid;
+  
+  if (uid == null) return []; 
 
-    for (var doc in snapshot.docs) {
-      final data = doc.data();
-      data['id'] = doc.id;
-      listHistory.add(data);
-    }
-    return listHistory;
+  final db = FirebaseFirestore.instance;
+  
+  final snapshot = await db
+      .collection('users')
+      .doc(uid)              
+      .collection('history') 
+      .orderBy('date', descending: true)
+      .get();
+  
+  List<Map<String, dynamic>> history = [];
+  for (var doc in snapshot.docs) {
+    final data = doc.data();
+    data['id'] = doc.id;
+    history.add(data);
   }
+  return history;
+}
 
   @override
   Future<int> counter() async {
